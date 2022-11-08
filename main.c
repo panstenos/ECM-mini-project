89bc6dfa228d7f88c257a6ee79b7928285012885
panstenos <112823396+panstenos@users.noreply.github.com>
2022-11-08 19:56:12 +0000

    LCD Working!

--- main.c
+++ main.c
@@ -13,51 +13,49 @@
 #include "LCD.h"
 
 #define _XTAL_FREQ 64000000 //note intrinsic _delay function is 62.5ns at 64,000,000Hz  
-/*
-#define _TEST_MODE 1
-#define _CURRENT_MINUTE 59 // from 0 to 59
-#define _CURRENT_HOUR 23 // from 0 to 23
-#define _DAY_OF_THE_WEEK 1 // 1 for monday, 7 for sunday
-#define _CURRENT_DAY 06 //Current day on the installation (from 1 to 30/31 depending on the month)
-#define _CURRENT_MONTH 11 //Current month on the installation (from 1 to 12)
-#define _CURRENT_YEAR 2022 //Current year
-*/
+
+int seconds = 0;
 
 void main(void) {
 	//call your initialisation functions to set up the hardware modules
-    //LEDarray_init();
+    //LEDarray_init();  
     Timer0_init();
     Interrupts_init();
     LCD_Init();
-    //char *lst[8] = {"Mon","30","01","2002","23","12","43","232"};
-    int *Sec;
-    int *Min;
-    int *Hou;
-    int *Day;
-    int *Week_day;
-    int *Mon;
-    int *Yea;
-    char day_of_the_week[7] = {'Mon','Tue','Wed','Thu','Fri','Sat','Sun'};
+    ADC_init();
     
-    char *Sec2;
-    char *Min2;
-    char *Hou2;
-    char *Day2;
-    char *Week_day2;
-    char *Mon2;
-    char *Yea2;
+    char Sec[2];
+    char Min[2];
+    char Hou[2];
+    char Day[2];
+    char Mon[2];
+    char Yea[4];
+    char ADC[3];
+    char day_of_the_week[] = {"MON","TUE","WED","THU","FRI","SAT","SUN"};
     while (1) {
-        //LCD_clear();
-        get(*Sec,*Min,*Hou,*Day,*Week_day,*Mon,*Yea);
-        ADC2String(*Sec2, Sec, 2);
-        ADC2String(*Min2, Min, 2);
-        ADC2String(*Hou2, Hou, 2);
-        ADC2String(*Day2, Sec, 2);
-        ADC2String(*Mon2, Mon, 2);
-        ADC2String(*Yea2, Yea, 4);
-        char *day_name = day_of_the_week[*Week_day];
-        char *lst[8] = {day_name,Day,Mon,Yea,Hou,Min,Sec,"123"};
+        LCD_clear();
+        //get(*Sec,*Min,*Hou,*Day,*Week_day,*Mon,*Yea);
+        //ADC2String(*Sec2, Sec, 2);
+        //ADC2String(*Min2, Min, 2);
+        //ADC2String(*Hou2, Hou, 2);
+        //ADC2String(*Day2, Mon, 2);
+        //ADC2String(*Mon2, Mon, 2);
+        //ADC2String(*Yea2, Yea, 4);
+        //ADC2String(*ADC, ADC_getval(),3);
+        //char *day_name = day_of_the_week[Week_day];
+        //char *lst[9] = {"day_name","Day2","Mon2","Yea2","Hou2","Min2","Sec2","ADC"};
+        //ADC2String(*Sec2, seconds, 2);
+        //char str[2];
+        ADC2String(Sec, get_seconds(), 2);
+        ADC2String(Min, get_minutes(), 2);
+        ADC2String(Hou, get_hours(), 2);
+        ADC2String(Day, get_day(), 2);
+        ADC2String(Mon, get_month()+1, 2);
+        ADC2String(Yea, get_year(), 4);   
+        ADC2String(ADC, ADC_getval(), 3);
+        
+        char *lst[8] = {get_week_day(),Day,Mon,Yea,Hou,Min,Sec,ADC};
         LCD_sendstring(lst);
-        __delay_ms(200);
+        __delay_ms(100);
     }
 }
