8eff3b67b8ca26dedfb3110e2138325f0ff41546
panstenos <112823396+panstenos@users.noreply.github.com>
2022-11-08 16:17:49 +0000

    New Code

--- interrupts.c
+++ interrupts.c
@@ -7,15 +7,11 @@
  * Note you also need to enable peripheral interrupts in the INTCON register to use CM1IE.
 ************************************/
 void Interrupts_init(void)
-{
-	// turn on global interrupts, peripheral interrupts and the interrupt source 
-	// It's a good idea to turn on global interrupts last, once all other interrupt configuration is done.
-    PIE2bits.C1IE = 1;
-    INTCONbits.IPEN = 1;
-    INTCONbits.GIEL=1;
-    IPR2bits.C1IP=1;
-    PIE0bits.TMR0IE=1; //Enabling the timer overflow interrupter
-    INTCONbits.GIEH=1;
+{   
+    INTCONbits.PEIE = 1;    //turns on all the peripheral interrupts
+    PIE0bits.TMR0IE = 1;    // enable Timer0 interrupt
+    PIE2bits.C1IE = 1;    //enable the comparator interrupt bit
+    INTCONbits.GIE = 1; 	//turn on interrupts globally (when this is off, all interrupts are deactivated)    
 }
 
 /************************************
@@ -23,25 +19,13 @@ void Interrupts_init(void)
  * Make sure all enabled interrupts are checked and flags cleared
 ************************************/
 void __interrupt(high_priority) HighISR()
-{
-	if(PIR2bits.C1IF){ 					//check the interrupt source
-        LATDbits.LATD7 = !LATDbits.LATD7;
-        PIR2bits.C1IF=0; 						//clear the interrupt flag!
+{   
+    if(PIR0bits.TMR0IF == 1){//check the interrupt flag
+        LATHbits.LATH3 = !LATHbits.LATH3; //toggle the LED
+        increment_seconds(); // call increment seconds function and add 1
+        // set the timer to reset at 3035 every time the it overflows
+        TMR0H=0b00001011;            
+        TMR0L=0b11011011;
+        PIR0bits.TMR0IF = 0; // turn flag off
 	}
-    if(PIR0bits.TMR0IF){ // Timer overflow flag
-        
-        increment_seconds(1);
-        
-        if(test_mode == 0){
-            TMR0H = 0b1011;
-            TMR0L = 0b11011011;
-        }else{
-            TMR0H = 0;
-            TMR0L = 0;
-        }
-        
-        PIR0bits.TMR0IF = 0;
-    }
-    
 }
-
