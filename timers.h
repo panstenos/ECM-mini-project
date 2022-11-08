2d63142cdacc555821fa18c61c013864f0535e17
panstenos <112823396+panstenos@users.noreply.github.com>
2022-11-08 16:18:45 +0000

    New Code

--- timers.h
+++ timers.h
@@ -5,21 +5,9 @@
 
 #define _XTAL_FREQ 64000000
 
-void Timer0_init(unsigned short,unsigned long, unsigned long, unsigned int, unsigned int, unsigned int,unsigned int);
+void Timer0_init(void);
+void get(int *Sec, int *Min, int *Hou, int *Day, int *Week_day, int *Mon, int *Yea);
 unsigned int get16bitTMR0val(void);
-unsigned short test_mode;
-
-unsigned int get_seconds(void);
-unsigned int get_minutes(void);
-unsigned int get_hours(void);
-unsigned int get_day(void);
-unsigned int get_month(void);
-
-void increment_seconds(unsigned int);
-void increment_minutes(unsigned int);
-void increment_hours(unsigned int);
-void increment_day(unsigned int);
-void increment_month(unsigned int);
-
-
+void increment_seconds(void);
+unsigned int check_for_hour_shift(void);
 #endif
