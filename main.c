b7e325541eb01a7c58b6c5c0d37e126ab04e8659
panstenos <112823396+panstenos@users.noreply.github.com>
2022-11-08 16:19:28 +0000

    New Code

--- main.c
+++ main.c
@@ -8,57 +8,56 @@
 #include <xc.h>
 #include "LEDarray.h"
 #include "interrupts.h"
-#include "comparator.h"
 #include "timers.h"
 #include "ADC.h"
 #include "LCD.h"
 
 #define _XTAL_FREQ 64000000 //note intrinsic _delay function is 62.5ns at 64,000,000Hz  
-
+/*
 #define _TEST_MODE 1
-#define _CURRENT_MINUTE 57 // from 0 to 59
-#define _CURRENT_HOUR 17 // from 0 to 23
+#define _CURRENT_MINUTE 59 // from 0 to 59
+#define _CURRENT_HOUR 23 // from 0 to 23
 #define _DAY_OF_THE_WEEK 1 // 1 for monday, 7 for sunday
 #define _CURRENT_DAY 06 //Current day on the installation (from 1 to 30/31 depending on the month)
 #define _CURRENT_MONTH 11 //Current month on the installation (from 1 to 12)
 #define _CURRENT_YEAR 2022 //Current year
+*/
 
 void main(void) {
 	//call your initialisation functions to set up the hardware modules
-    LATHbits.LATH3=0;   //set initial output statem different than the other diode for first task
-    TRISHbits.TRISH3=0; //set TRIS value for pin (output)
-    
-    TRISDbits.TRISD7=0;
-    LATDbits.LATD7=1;
-    
-    TRISAbits.TRISA3=1;
-    
-    LATHbits.LATH3 = 1;
-    Comp1_init();
+    //LEDarray_init();
+    Timer0_init();
     Interrupts_init();
-    Timer0_init(_TEST_MODE,_CURRENT_MINUTE,_CURRENT_HOUR,_CURRENT_DAY,_DAY_OF_THE_WEEK,_CURRENT_MONTH,_CURRENT_YEAR);
-    LEDarray_init();
-    ADC_init();
     LCD_Init();
-
-    unsigned int curr_day;
-    unsigned int curr_month;
-
+    //char *lst[8] = {"Mon","30","01","2002","23","12","43","232"};
+    int *Sec;
+    int *Min;
+    int *Hou;
+    int *Day;
+    int *Week_day;
+    int *Mon;
+    int *Yea;
+    char day_of_the_week[7] = {'Mon','Tue','Wed','Thu','Fri','Sat','Sun'};
+    
+    char *Sec2;
+    char *Min2;
+    char *Hou2;
+    char *Day2;
+    char *Week_day2;
+    char *Mon2;
+    char *Yea2;
     while (1) {
-        unsigned int curr_hour = get_hours();
-        LEDarray_disp_bin(curr_hour);
-        
-        if(curr_hour >= 1 && curr_hour <= 5){
-            LATHbits.LATH3 = 0;
-        }else{
-            LATHbits.LATH3 = 1;
-        }
-        
-        
-        display_informations(ADC_getval());
-        
-        
-        __delay_ms(100);
-        LCD_clear();
+        //LCD_clear();
+        get(*Sec,*Min,*Hou,*Day,*Week_day,*Mon,*Yea);
+        ADC2String(*Sec2, Sec, 2);
+        ADC2String(*Min2, Min, 2);
+        ADC2String(*Hou2, Hou, 2);
+        ADC2String(*Day2, Sec, 2);
+        ADC2String(*Mon2, Mon, 2);
+        ADC2String(*Yea2, Yea, 4);
+        char *day_name = day_of_the_week[*Week_day];
+        char *lst[8] = {day_name,Day,Mon,Yea,Hou,Min,Sec,"123"};
+        LCD_sendstring(lst);
+        __delay_ms(200);
     }
 }
