f6e715167f8dc82ac5d1a5ef4298943b0b68de83
panstenos <112823396+panstenos@users.noreply.github.com>
2022-11-10 00:01:29 +0000

    Exercise 6 improved + comments
    Added Comments
    Removed non used functions

--- interrupts.c
+++ interrupts.c
@@ -22,7 +22,8 @@ void Interrupts_init(void)
 void __interrupt(high_priority) HighISR()
 {   
     if(PIR0bits.TMR0IF == 1){//check the interrupt flag
-    increment_seconds();        // set the timer to reset at 3035 every time the it overflows
+    increment_seconds(); //call the function to increment the seconds
+    // set the timer to reset at 3035 every time the it overflows
     if(test_mode == 0){
             TMR0H=0b00001011;            
             TMR0L=0b11011011;
